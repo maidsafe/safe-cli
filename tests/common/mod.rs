14bcd1595f406f71bf131c447f7e909f1e53dc87
Chris O'Neil <chris.oneil@gmail.com>
2019-07-02 13:41:47 +0100

    fix(ci): include using statement
    This was accidentally not included after a merge conflict.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -5,6 +5,7 @@
 // under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
+use std::env;
 
 pub const CLI: &str = "safe";
 #[allow(dead_code)]
