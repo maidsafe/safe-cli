d8aae782167c8a99ab873a82dad8a15168ffccdf
Gabriel Viganotti <gabrielviganotti@gmail.com>
2020-03-10 18:50:02 -0300

    refactor/cli: use tokio macros for async tests (tokio::test) and for cli main function (tokio::main)

--- safe-cli/operations/auth_and_connect.rs
+++ safe-cli/operations/auth_and_connect.rs
@@ -22,7 +22,7 @@ use std::{
 
 const AUTH_CREDENTIALS_FILENAME: &str = "credentials";
 
-pub fn authorise_cli(
+pub async fn authorise_cli(
     _safe: &mut Safe,
     endpoint: Option<String>,
     is_self_authing: bool,
@@ -33,8 +33,14 @@ pub fn authorise_cli(
         println!("Note you can use this CLI from another console to authorise it with 'auth allow' command. Alternativelly, you can also use '--self-auth' flag with 'auth login' command to automatically self authorise the CLI app.");
     }
     println!("Awaiting for authorising response from authd...");
-    let auth_credentials = Safe::auth_app(APP_ID, APP_NAME, APP_VENDOR, endpoint.as_deref())
-        .map_err(|err| format!("Application authorisation failed: {}", err))?;
+    let auth_credentials = Safe::auth_app(
+        APP_ID,
+        APP_NAME,
+        APP_VENDOR,
+        endpoint.as_ref().map(String::as_str),
+    )
+    .await
+    .map_err(|err| format!("Application authorisation failed: {}", err))?;
 
     file.write_all(auth_credentials.as_bytes()).map_err(|err| {
         format!(
