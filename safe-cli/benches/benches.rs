ebef244a062d3773447eb551797f7802cf33a8b4
Josh Wilson <joshuef@gmail.com>
2020-03-12 10:46:06 +0100

    use criterion

--- safe-cli/benches/benches.rs
+++ safe-cli/benches/benches.rs
@@ -1,8 +1,8 @@
-#![feature(test)]
+// #![feature(test)]
 
 
-extern crate safe_utilities;
-extern crate test;
+// extern crate safe_utilities;
+// extern crate test;
 #[macro_use]
 extern crate duct;
 
@@ -14,6 +14,8 @@ use safe_utilities::{
 use assert_cmd::prelude::*;
 const TEST_FILE: &str = "../testdata/test.md";
 
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
 pub fn calling_safe_cat() -> () {
     let content = cmd!(get_bin_location(), "files", "put", TEST_FILE, "--json")
         .read()
@@ -22,16 +24,30 @@ pub fn calling_safe_cat() -> () {
     // safe_utilities::shared_code();
 }
 
-#[cfg(test)]
-mod tests {
-        use super::*;
-        use test::Bencher;
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n-1) + fibonacci(n-2),
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//         use super::*;
+        // use test::Bencher;
         // use tests::cli_cat::calling_safe_cat;
         // use tests::calling_safe_cat;
         
-        #[bench]
-        fn bench_safe_cat(b: &mut Bencher) {
-            b.iter(|| calling_safe_cat());
+        // #[bench]
+        // fn c: &mut Criterion(b: &mut Bencher) {
+        //     b.iter(|| calling_safe_cat());
+        // }
+
+        fn criterion_benchmark(c: &mut Criterion) {
+            c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
         }
     
-}
+        criterion_group!(benches, criterion_benchmark);
+        criterion_main!(benches);
+// }
